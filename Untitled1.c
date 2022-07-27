5429fb6982d4719dfb6f44f0e19e688d661a3a06
anand2piyush <110121419+anand2piyush@users.noreply.github.com>
2022-07-27 22:41:54 +0530

    1st

---
+++ Untitled1.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int l,b,a;
+ printf("Enter the length of rectangle:  ");
+scanf("%d",&l);
+
+printf("\nEnter the breadth of rectangle: ");
+scanf("%d", &b);
+
+a= l*b;
+printf("\nThe area of rectangle is %d", a);
+
+return 0;
+
+}
